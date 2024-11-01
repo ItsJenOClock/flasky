a6a3efb1ac1b73a4ed49488302b60d575b48791b
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-31 21:20:28 -0400

    edited tests

--- tests/test_cat_routes.py
+++ tests/test_cat_routes.py
@@ -5,30 +5,46 @@ def test_get_all_cats_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-def test_get_one_cat_success(client, two_saved_cats):
+def test_get_one_cat_succeeds(client, two_saved_cats):
     response = client.get("cats/1")
-    response_body = response.get_json()
+    response_body= response.get_json()
 
     assert response.status_code == 200
     assert response_body == {
         "id": 1,
-        "name": "Luna",
+        "name":"Luna",
         "color": "ash",
         "personality": "Tomato Queen"
     }
 
-def test_create_one_cat(client):
+def test_create_one_cat_in_empty_db(client):
     response = client.post("/cats", json={
         "name": "Garfield",
         "color": "Orange",
-        "personality": "Likes lasagna, hates Mondays"
+        "personality": "Likes Lasagna, hates Mondays"
     })
     response_body = response.get_json()
-    
+
     assert response.status_code == 201
     assert response_body == {
         "id": 1,
         "name": "Garfield",
         "color": "Orange",
-        "personality": "Likes lasagna, hates Mondays"
+        "personality": "Likes Lasagna, hates Mondays"
+    }
+
+def test_create_one_cat_with_cats_already_in_db(client, two_saved_cats):
+    response = client.post("/cats", json={
+        "name": "Garfield",
+        "color": "Orange",
+        "personality": "Likes Lasagna, hates Mondays"
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 3,
+        "name": "Garfield",
+        "color": "Orange",
+        "personality": "Likes Lasagna, hates Mondays"
     }
