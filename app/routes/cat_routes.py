639ff08f56851a58ea0d3735eed8df948ff6bb60
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-04 13:52:03 -0500

    refactored and created from_dict()

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -9,16 +9,16 @@ cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
 @cats_bp.post("")
 def create_cat():
     request_body = request.get_json()
-    name = request_body["name"]
-    color = request_body["color"]
-    personality = request_body["personality"]
+    try: 
+        new_cat = Cat.from_dict(request_body)
+        db.session.add(new_cat)
+        db.session.commit()
 
-    new_cat = Cat(name=name, color=color, personality=personality)
-    db.session.add(new_cat)
-    db.session.commit()
-
-    response = new_cat.to_dict()
-    return response, 201
+        response = new_cat.to_dict()
+        return response, 201
+    except KeyError as e:
+        response = {"message": f"Invalid Request: missing {e.args[0]}"}
+        abort(make_response(response, 400))
 
 @cats_bp.get("")
 def get_all_cats():
