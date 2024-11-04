4b0d0a7d29c98da26d49564e9e1e335e9a6ea281
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-04 14:31:53 -0500

    refactored

--- app/models/cat.py
+++ app/models/cat.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from ..db import db, migrate
+from ..db import db
 
 class Cat(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, abort, make_response, request, Response
 from ..db import db
 from app.models.cat import Cat
+from .route_utilities import validate_model
 
 # curl -X POST -H 'Content-Type: application/json' -d '{"name":"Felix","color":"black and white","personality":"wonderful"}' 'http://localhost:5000/cats' | json_pp
 
@@ -11,14 +12,14 @@ def create_cat():
     request_body = request.get_json()
     try: 
         new_cat = Cat.from_dict(request_body)
-        db.session.add(new_cat)
-        db.session.commit()
-
-        response = new_cat.to_dict()
-        return response, 201
     except KeyError as e:
         response = {"message": f"Invalid Request: missing {e.args[0]}"}
         abort(make_response(response, 400))
+    
+    db.session.add(new_cat)
+    db.session.commit()
+    response = new_cat.to_dict()
+    return response, 201
 
 @cats_bp.get("")
 def get_all_cats():
@@ -46,13 +47,13 @@ def get_all_cats():
 
 @cats_bp.get("/<cat_id>")
 def get_single_cat(cat_id):
-    cat = validate_cat(cat_id)
+    cat = validate_model(Cat, cat_id)
 
     return cat.to_dict()
 
 @cats_bp.put("/<cat_id>")
 def update_cat(cat_id):
-    cat = validate_cat(cat_id)
+    cat = validate_model(Cat, cat_id)
     request_body = request.get_json()
 
     cat.name = request_body["name"]
@@ -65,24 +66,9 @@ def update_cat(cat_id):
 
 @cats_bp.delete("/<cat_id>")
 def delete_cat(cat_id):
-    cat = validate_cat(cat_id)
+    cat = validate_model(Cat, cat_id)
 
     db.session.delete(cat)
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
-
-
-def validate_cat(cat_id):
-    try:
-        cat_id = int(cat_id)
-    except:
-        abort(make_response({"message":f"Cat id {cat_id} invalid"}, 400))
-    
-    query = db.select(Cat).where(Cat.id == cat_id)
-    cat = db.session.scalar(query)
-
-    if not cat:
-        abort(make_response({ "message": f"Cat {cat_id} not found"}, 404))
-    
-    return cat
