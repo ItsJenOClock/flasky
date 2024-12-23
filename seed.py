6e7ec53b643b01e2457c4e021415d100b8afc1d9
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-12-23 04:19:11 -0600

    updated requirements and seed file

--- seed.py
+++ seed.py
@@ -1,41 +1,36 @@
+# place in a top-level file. call it something like seed.py
+# no need to dwell on the `with my_app.app_context():`, other than to say
+# that the `db` reference won't work unless it runs with an app context
+
 from app import create_app, db
 from app.models.cat import Cat
+from app.models.caretaker import Caretaker
 
 my_app = create_app()
+
 with my_app.app_context():
-    db.session.add(Cat(name="Luna", color="ash", personality="Tomato Queen")),
-    db.session.add(Cat(name="Simon", color="black", personality="might be a human stuck in a cats body")),
+    db.session.add(Caretaker(name="Jill")),
+    db.session.add(Caretaker(name="Jasmine")),
+    db.session.add(Caretaker(name="Monica")),
+    db.session.add(Caretaker(name="Fantasia")),
+    db.session.add(Caretaker(name="Lauryn")),
+    db.session.add(Caretaker(name="Janet")),
+    db.session.add(Cat(name="Luna", color="ash", caretaker_id=1, personality="Tomato Queen")),
+    db.session.add(Cat(name="Simon", color="black", caretaker_id=2, personality="might be a human stuck in a cats body")),
     db.session.add(Cat(name="Midnight", color="black", personality="skittish")),
-    db.session.add(Cat(name="Leo", color="gray tabby", personality="friendly")),
+    db.session.add(Cat(name="Leo", color="gray tabby", caretaker_id=4, personality="friendly")),
     db.session.add(Cat(name="Ash", color="gray", personality="cranky")),
     db.session.add(Cat(name="Alder", color="auburn", personality="bouncy, trouncy, flouncy, pouncy, fun, fun, fun, fun, fun")),
-    db.session.add(Cat(name="Morty", color="orange", personality="orange")),
-    db.session.add(Cat(name="fluffy", color="white", personality="evil with a hint of benevolent")),
+    db.session.add(Cat(name="Morty", color="orange", caretaker_id=3, personality="orange")),
+    db.session.add(Cat(name="fluffy", color="white", caretaker_id=3, personality="evil with a hint of benevolent")),
     db.session.add(Cat(name="Reginold", color="orange", personality="only has one brain cell, but is descendent of Reginold the Great Tabby")),
-    db.session.add(Cat(name="Katosa", color="gray tabby", personality="Crazy Hunter")),
+    db.session.add(Cat(name="Katosa", color="gray tabby", caretaker_id=6, personality="Crazy Hunter")),
     db.session.add(Cat(name="Milly", color="Tortoiseshell", personality="Loves you a lot but will probably sneeze all over you")),
-    db.session.add(Cat(name="Meryl", color="Tortoiseshell", personality="Bossy but tries to pass as the sweet one")),
-    db.session.add(Cat(name="Zelda", color="white, gray", personality="a mystery")),
+    db.session.add(Cat(name="Meryl", color="Tortoiseshell", caretaker_id=5, personality="Bossy but tries to pass as the sweet one")),
+    db.session.add(Cat(name="Zelda", color="white, gray", caretaker_id=2, personality="a mystery")),
     db.session.add(Cat(name="Jupiter", color="orange", personality="socially selective")),
     db.session.add(Cat(name="Neo", color="black", personality="stoic")),
-    db.session.add(Cat(name="Gato", color="grey", personality="fun")),
+    db.session.add(Cat(name="Gato", color="grey", caretaker_id=4, personality="fun")),
     db.session.add(Cat(name="Red XIII", color="red", personality="serious")),
-    db.session.add(Cat(name="Gizzy", color="white", personality="unbothered")),
+    db.session.add(Cat(name="Gizzy", color="white", caretaker_id=5, personality="unbothered")),
     db.session.commit()
-
-# cats = [
-#     Cat(1, "Luna", "black/white", "lazy"),
-#     Cat(2, "Simon", "black", "might be a human stuck in a cat's body"),
-#     Cat(3, "Midnight", "black", "skittish"),
-#     Cat(4, "Leo", "gray tabby", "friendly"),
-#     Cat(5, "Ash", "gray", "cranky"),
-#     Cat(6, "Alder", "auburn", "bouncy, trouncy, flouncy, pouncy, fun, fun, fun, fun, fun"),
-#     Cat(7, "Morty", "orange", "orange"),
-#     Cat(8, "fluffy", "white", "evil with a hint of benevolent"),
-#     Cat(9, "Reginold", "orange", "only has one brain cell, but is descendent of Reginold the Great Tabby"),
-#     Cat(10, "Katosa", "gray tabby", "Crazy Hunter"),
-#     Cat(11, "Milly", "Tortoiseshell", "Loves you a lot but will probably sneeze all over you"),
-#     Cat(12, "Meryl", "Tortoiseshell", "Bossy but tries to pass as the sweet one"),
-#     Cat(13, "Zelda", "white, gray", "a mystery"),
-#     Cat(14, "Jupiter", "orange", "socially selective"),
-# ]
